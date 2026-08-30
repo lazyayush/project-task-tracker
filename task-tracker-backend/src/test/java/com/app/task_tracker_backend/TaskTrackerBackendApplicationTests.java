191fb99f20bfbb3c788d00f8cbda337ffd16fdc1
Ayush Prajapati <prajapatiayush1921@gmail.com>
2026-08-30 14:35:13 +0530

    initial commit

---
+++ task-tracker-backend/src/test/java/com/app/task_tracker_backend/TaskTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.task_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
